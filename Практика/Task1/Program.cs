f1f5e74abb7baefb7110caab6b9a10839ea95831
Nikolay <nikola05@list.ru>
2022-06-27 17:53:26 +0300

    дз

---
+++ Практика/Task1/Program.cs
@@ -0,0 +1,42 @@
+﻿// адача 1: Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. Найдите сумму отрицательных и положительных элементов массива.
+void Array(int[] dlina)
+{
+    int length = dlina.Length;
+    int index = 0;
+    while (index < length)
+    {
+        dlina[index] = new Random().Next(-9, 10);
+        index++;
+    }
+}
+
+int[] mass = new int[12];
+Array(mass);
+int a = 0;
+int b = 0;
+int count = 0;
+while (count < mass.Length)
+{
+    if (mass[count] > 0)
+    {
+        a = a +mass[count];
+        count++;
+    }
+    else
+    {
+        b = b +mass[count];
+        count++;
+    }
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position ++;
+    }
+}
+PrintArray(mass);
+Console.WriteLine( "сумма положительных чисел в массиве равна " + a + " сумма отрицательных чисел в массиве равна "+b );
---
+++ Практика/Task2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 1: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+void GreateMass(int[] a)
+{
+    int lenght = a.Length;
+    int index = 0;
+    while (index < lenght)
+{
+    a[index] = new Random().Next(-10, 11);
+    index++;
+}
+
+}
+
+int[] mass = new int[5];
+int a = 0;
+GreateMass(mass);
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position ++;
+    }
+}
+PrintArray(mass);
+int b = mass.Length;
+while (a < b)
+{
+    mass[a] = mass[a] * -1;
+    a++;
+}
+
+PrintArray(mass);
